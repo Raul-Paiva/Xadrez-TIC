f6a9b7ec9372b6a3c9c69e95521ca9c015913d6b
Raul Paiva <rejphp@gmail.com>
2024-05-27 22:02:54 +0100

    Implementei a jogada especial EnPassant Possível erro no método UnmovePiece por causa da jogada implementada.

--- Chess/ChessMatch.cs
+++ Chess/ChessMatch.cs
@@ -24,8 +24,8 @@ namespace Xadrez_TIC.Chess
             PlayerColor = Color.White;
             finished = false;
             xeque = false;
-            EnPassant = null;
             captured = new HashSet<Piece>();
+            EnPassant = null;
         }
 
         public void CheckPositionOrigin(Position pos)
@@ -53,7 +53,7 @@ namespace Xadrez_TIC.Chess
             // Promotion --------------------------------------------------\\
             if (p is Peao && ((p.color == Color.White && destination.row == 7) || (p.color == Color.Black && destination.row == 0)))
             {
-                Promotion promoting = new Promotion(this,p);
+                Promotion promoting = new Promotion(this, p);
                 promoting.Promote();
             }
 
@@ -82,17 +82,48 @@ namespace Xadrez_TIC.Chess
                     PlayerColor = Color.White;
                 }
             }
-            /*
-            // #jogadaespecial en passant
-            if (p is Peao && (destination.row == origin.row - 2 || destination.row == origin.row + 2))
+
+            //----------------------------------------------\\
+            //---------- Special Move - EnPassant ----------\\
+            //----------------------------------------------\\
+            if (p is Peao && (destination.row == origin.row + 2 || destination.row == origin.row - 2))
             {
                 EnPassant = p;
             }
             else
             {
                 EnPassant = null;
-            }*/
+            }
+            //--------------------------------------------------\\
+        }
 
+        //----------------------------------------------\\
+        //---------- Special Move - EnPassant ----------\\
+        //----------------------------------------------\\ Nao esta a ser usado
+        public bool IsCaptingWithEnPassant(Position origin, Position destination, Color pieceColor)
+        {
+            if (pieceColor == Color.Black)
+            {
+                //Esta a mover para a posicao da peca que devia ser capturada
+                if (destination.row == origin.row - 1 && (destination.column == origin.column - 1 || destination.column == origin.column + 1))
+                {
+                    Piece pieceToBeCaptured = tab.PiecesPos[destination.row + 1, destination.column];
+                    if (pieceToBeCaptured is Peao && pieceToBeCaptured.color != pieceColor && pieceToBeCaptured == EnPassant)
+                    { return true; }
+                }
+                return false;
+            }
+            else if (pieceColor == Color.White)
+            {
+                if (destination.row == origin.row + 1 && (destination.column == origin.column - 1 || destination.column == origin.column + 1))
+                {
+                    Piece pieceToBeCaptured = tab.PiecesPos[destination.row - 1, destination.column];
+                    if (pieceToBeCaptured is Peao && pieceToBeCaptured.color != pieceColor && pieceToBeCaptured == EnPassant)//Nao sei se funciona o ultimo
+                    { return true; }
+                }
+                return false;
+            }
+            else throw new FatalException("Erro nas Cores!");
         }
 
         private Piece MovePiece(Position origin, Position destination)
@@ -108,6 +139,28 @@ namespace Xadrez_TIC.Chess
                     captured.Add(capturedPiece);
                     capturedPiece.Captured();
                 }
+
+                //----------------------------------------------\\
+                //---------- Special Move - EnPassant ----------\\
+                //----------------------------------------------\\
+                if (p is Peao)
+                {
+                    if (origin.column != destination.column && capturedPiece is Free)
+                    {
+                        Position posPieceToCapture;
+                        if (p.color == Color.Black)
+                        {
+                            posPieceToCapture = new Position(destination.row + 1, destination.column);
+                        }
+                        else
+                        {
+                            posPieceToCapture = new Position(destination.row - 1, destination.column);
+                        }
+                        capturedPiece = tab.RemovePiece(posPieceToCapture);
+                        captured.Add(capturedPiece);
+                    }
+                }
+                //----------------------------------------------\\
 
 
                 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -133,46 +186,54 @@ namespace Xadrez_TIC.Chess
                     T.AddNMoves();
                     tab.AddPiece(destinoT, T);
                 }
-                //----------\\
-
-                //En Passant\\
-                if (p is Peao)
-                {
-                    if (origin.column != destination.column && capturedPiece == null)
-                    {
-                        Position posP;
-                        if (p.color == Color.White)
-                        {
-                            posP = new Position(destination.row + 1, destination.column);
-                        }
-                        else
-                        {
-                            posP = new Position(destination.row - 1, destination.column);
-                        }
-                        capturedPiece = tab.RemovePiece(posP);
-                        captured.Add(capturedPiece);
-                    }
-                }
                 //----------\\*/
-                //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
                 return capturedPiece;
             }
             else { tab.AddPiece(origin, p); throw new ChessException("Não é possível mover a peça para essa posição. Considera outra estratégia!"); }
 
         }
 
-        //Undo the move of a piece
+        /// <summary>
+        /// Undo the move of a piece.
+        /// </summary>
+        /// <param name="originalposorigin"></param>
+        /// <param name="originalposdestination"></param>
+        /// <param name="capturedPiece"></param>
         private void UnmovePiece(Position originalposorigin, Position originalposdestination, Piece capturedPiece)//tenho de fazer uns ajustes para desfazer os movimentos apenas em pecas recentes
         {
             Piece p = tab.RemovePiece(originalposdestination);//Remove the piece moved from it original destination position
             p.RemoveNMoves();//Remove the move from the nMoves list
-            if (capturedPiece is not Free && capturedPiece != null)//Tests if was captured any piece
+            if (capturedPiece is not Free || capturedPiece != null)//Tests if was captured any piece
             {
                 tab.AddPiece(originalposdestination, capturedPiece);//Add the piece captured to it original position
                 captured.Remove(capturedPiece);//Remove the caputred piece from the captured list
                 capturedPiece.UndoCapture();
             }
             tab.AddPiece(originalposorigin, p);//Add the moved piece to it original origin position
+
+            //----------------------------------------------\\
+            //---------- Special Move - EnPassant ----------\\
+            //----------------------------------------------\\
+            if (p is Peao && capturedPiece is not Free)
+            {
+                if (originalposorigin.column != originalposdestination.column && capturedPiece == EnPassant)// sera que a peca continua registrada no EnPassant(nao sei)
+                {
+                    //Piece peao = tab.RemovePiece(originalposdestination);
+                    Position posPieceToUndoCapture;
+                    if (p.color == Color.White)//esta ao contrrio mas esta bem(peca capturada e da cor oposta a peca movida)
+                    {
+                        posPieceToUndoCapture = new Position(3, originalposdestination.column);
+                    }
+                    else
+                    {
+                        posPieceToUndoCapture = new Position(4, originalposdestination.column);
+                    }
+                    tab.AddPiece(posPieceToUndoCapture, capturedPiece);
+                }
+            }
+            //-------------------------------------------------\\
 
 
 
@@ -198,27 +259,8 @@ namespace Xadrez_TIC.Chess
                 T.RemoveNMoves();
                 tab.AddPiece(origemT, T);
             }
-            //----------\\
-
-            // #jogadaespecial en passant
-            if (p is Peao)
-            {
-                if (originalposorigin.column != originalposdestination.column && capturedPiece == EnPassant)
-                {
-                    Piece peao = tab.RemovePiece(originalposdestination);
-                    Position posP;
-                    if (p.color == Color.White)
-                    {
-                        posP = new Position(3, originalposdestination.column);
-                    }
-                    else
-                    {
-                        posP = new Position(4, originalposdestination.column);
-                    }
-                    tab.AddPiece(posP, peao);
-                }
-            }*/
-            //----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //----------\\*/
+            //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
         }
 
         /// <summary>
--- Pieces/Peao.cs
+++ Pieces/Peao.cs
@@ -1,86 +1,115 @@
 ﻿using Xadrez_TIC.Chess;
 using Color = Xadrez_TIC.Enums.Color;
 
-namespace Xadrez_TIC.Pieces {
+namespace Xadrez_TIC.Pieces
+{
 
-    class Peao : Piece {
+    class Peao : Piece
+    {
 
-        private ChessMatch partida;
+        private ChessMatch match;
 
-        public Peao(Board tab, Color color, ChessMatch partida) : base(tab, color) {
-            this.partida = partida;
+        public Peao(Board tab, Color color, ChessMatch match) : base(tab, color)
+        {
+            this.match = match;
         }
 
-        public override string ToString() {
+        public override string ToString()
+        {
             return "peao";
         }
 
-        public override bool[,] PossibleMoves() {
+        public override bool[,] PossibleMoves()
+        {
             bool[,] mat = new bool[tab.rows, tab.columns];
 
             Position pos = new Position(0, 0);
 
-            if (color == Color.Black) {
+            if (color == Color.Black)
+            {
                 pos.DefineNewPositionValues(position.row - 1, position.column);
-                if (pos.IsPositionValid() && IsFree(pos)) {
+                if (pos.IsPositionValid() && IsFree(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
                 pos.DefineNewPositionValues(position.row - 2, position.column);
                 Position p2 = new Position(position.row - 1, position.column);
-                if (p2.IsPositionValid() && IsFree(p2) && pos.IsPositionValid() && IsFree(pos) && nMoves == 0) {
+                if (p2.IsPositionValid() && IsFree(p2) && pos.IsPositionValid() && IsFree(pos) && nMoves == 0)
+                {
                     mat[pos.row, pos.column] = true;
                 }
                 pos.DefineNewPositionValues(position.row - 1, position.column - 1);
-                if (pos.IsPositionValid() && IsFreeToMove(pos)) {
+                if (pos.IsPositionValid() && IsFreeToMove(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
                 pos.DefineNewPositionValues(position.row - 1, position.column + 1);
-                if (pos.IsPositionValid() && IsFreeToMove(pos)) {
+                if (pos.IsPositionValid() && IsFreeToMove(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
-                /*
-                // #jogadaespecial en passant
-                if (position.row == 3) {
-                    Position esquerda = new Position(position.row, position.column - 1);
-                    if (esquerda.IsPositionValid() && IsFreeToMove(esquerda) && tab.PiecePosition(esquerda) == partida.EnPassant) {
-                        mat[esquerda.row - 1, esquerda.column] = true;
+
+                //----------------------------------------------\\
+                //---------- Special Move - EnPassant ----------\\
+                //----------------------------------------------\\
+                if (position.row == 3)
+                {
+                    Position left = new Position(position.row - 1, position.column - 1);
+                    if (left.IsPositionValid() && IsFreeToMove(new Position(left.row + 1, left.column)) && IsFree(left) && match.IsCaptingWithEnPassant(position, left, color))
+                    {  //Não funcionou match.IsCaptingWithEnPassant(position, left, color) <- Preferia assim
+                        mat[left.row, left.column] = true;
                     }
-                    Position direita = new Position(position.row, position.column + 1);
-                    if (direita.IsPositionValid() && IsFreeToMove(direita) && tab.PiecePosition(direita) == partida.EnPassant) {
-                        mat[direita.row - 1, direita.column] = true;
+
+                    Position right = new Position(position.row - 1, position.column + 1);
+                    if (right.IsPositionValid() && IsFreeToMove(new Position(right.row + 1, right.column)) && IsFree(right) && match.IsCaptingWithEnPassant(position, right, color))
+                    {
+                        mat[right.row, right.column] = true;
                     }
-                }*/
+                }
+                //----------------------------------------------\\
             }
-            else {
+            else
+            {
                 pos.DefineNewPositionValues(position.row + 1, position.column);
-                if (pos.IsPositionValid() && IsFree(pos)) {
+                if (pos.IsPositionValid() && IsFree(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
-                pos.DefineNewPositionValues(position.row + 2, position.column);//Erro
+                pos.DefineNewPositionValues(position.row + 2, position.column);
                 Position p2 = new Position(position.row + 1, position.column);
-                if (p2.IsPositionValid() && IsFree(p2) && pos.IsPositionValid() && IsFree(pos) && nMoves == 0) {
+                if (p2.IsPositionValid() && IsFree(p2) && pos.IsPositionValid() && IsFree(pos) && nMoves == 0)
+                {
                     mat[pos.row, pos.column] = true;
                 }
                 pos.DefineNewPositionValues(position.row + 1, position.column - 1);
-                if (pos.IsPositionValid() && IsFreeToMove(pos)) {
+                if (pos.IsPositionValid() && IsFreeToMove(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
                 pos.DefineNewPositionValues(position.row + 1, position.column + 1);
-                if (pos.IsPositionValid() && IsFreeToMove(pos)) {
+                if (pos.IsPositionValid() && IsFreeToMove(pos))
+                {
                     mat[pos.row, pos.column] = true;
                 }
-                /*
-                // #jogadaespecial en passant
-                if (position.row == 4) {
-                    Position esquerda = new Position(position.row, position.column - 1);
-                    if (esquerda.IsPositionValid() && IsFreeToMove(esquerda) && tab.PiecePosition(esquerda) == partida.EnPassant) {
-                        mat[esquerda.row + 1, esquerda.column] = true;
+
+                //----------------------------------------------\\
+                //---------- Special Move - EnPassant ----------\\
+                //----------------------------------------------\\
+                if (position.row == 4)
+                {
+                    Position left = new Position(position.row + 1, position.column - 1);
+                    if (left.IsPositionValid() && IsFreeToMove(new Position(left.row - 1, left.column)) && IsFree(left) && match.IsCaptingWithEnPassant(position, left, color))
+                    {
+                        mat[left.row, left.column] = true;
                     }
-                    Position direita = new Position(position.row, position.column + 1);
-                    if (direita.IsPositionValid() && IsFreeToMove(direita) && tab.PiecePosition(direita) == partida.EnPassant) {
-                        mat[direita.row + 1, direita.column] = true;
+
+                    Position right = new Position(position.row + 1, position.column + 1);
+                    if (right.IsPositionValid() && IsFreeToMove(new Position(right.row - 1, right.column)) && IsFree(right) && match.IsCaptingWithEnPassant(position, right, color))
+                    {
+                        mat[right.row, right.column] = true;
                     }
-                }*/
+                }
+                //----------------------------------------------\\
             }
 
             return mat;
